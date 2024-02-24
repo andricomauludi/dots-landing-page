ea9cf4694eaef8e6f6abe61666959dacffcbb0c9
andricomauludi <mauludi21@gmail.com>
2024-02-24 17:13:04 +0700

    making card for member

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,6 +1,20 @@
-window.onscroll = function() {
-    var title = this.document.getElementById("anim-mask");
-    var scrollTop = document.documentElement.scrollTop * 0.5;
-    title.style.transform =
-      "translateX(-" + scrollTop + "px) translateY(-" + scrollTop + "px)";
-  };
+window.onscroll = function () {
+  // var elementTarget = document.getElementById("buttoncoba");
+  // if (window.scrollY > elementTarget.offsetTop + elementTarget.offsetHeight) {
+  //   var title = this.document.getElementById("anim-mask");
+  //   var scrollTop = document.documentElement.scrollTop * 0.5;
+  //   title.style.transform = "translateX(" + scrollTop + "px)";
+  // }
+
+  var testDivFromTop = document.getElementById("container-scroll").offsetTop;
+  var pageHeight = window.innerHeight;
+  if (
+    document.body.scrollTop > testDivFromTop - pageHeight ||
+    document.documentElement.scrollTop > testDivFromTop - pageHeight
+  ) {
+    var title = this.document.getElementById("card-scroll");
+    var scrollTop = document.documentElement.scrollTop * 0.1;
+    title.style.transform =      "translateX(" + scrollTop + "px) translateY( -" + scrollTop + "px)";
+    // title.style.transform =      "translateX(-" + scrollTop + "px) translateY( -" + scrollTop + "px)";
+  }
+};
